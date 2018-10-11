28e9e2a728c69218e3b4286a10436c0915fb5620
dmarulli <dmmarulli@gmail.com>
2018-10-11 16:28:47 -0700

    babel-ify

--- js/search.js
+++ js/search.js
@@ -1,38 +1,28 @@
 function addPlaceListener(instance, ref) {
-  instance[ref].addListener('place_changed', () => {
-    let place = instance[ref].getPlace();
-    let lat = place.geometry.location.lat();
-    let lng = place.geometry.location.lng();
-    instance.home_address = place.formatted_address
-    selectParcel(lat, lng, instance)
+  instance[ref].addListener('place_changed', function () {
+    var place = instance[ref].getPlace();
+    var lat = place.geometry.location.lat();
+    var lng = place.geometry.location.lng();
+    instance.home_address = place.formatted_address;
+    selectParcel(lat, lng, instance);
   });
-
 }
 
-
 function initializeAddressSearch(instance) {
-  instance.autocomplete0 = new google.maps.places.Autocomplete(
-      (instance.$refs.autocomplete0),
-      {types: ['geocode']}
-    );
-
-    instance.autocomplete1 = new google.maps.places.Autocomplete(
-      (instance.$refs.autocomplete1),
-      {types: ['geocode']}
-    );
-
-    instance.autocomplete2 = new google.maps.places.Autocomplete(
-      (instance.$refs.autocomplete2),
-      {types: ['geocode']}
-    );
-
-    instance.autocomplete3 = new google.maps.places.Autocomplete(
-      (instance.$refs.autocomplete3),
-      {types: ['geocode']}
-    );
-
-  addPlaceListener(instance, 'autocomplete0')
-  addPlaceListener(instance, 'autocomplete1')
-  addPlaceListener(instance, 'autocomplete2')
-  addPlaceListener(instance, 'autocomplete3')
-  }
+  instance.autocomplete0 = new google.maps.places.Autocomplete(instance.$refs.autocomplete0, {
+    types: ['geocode']
+  });
+  instance.autocomplete1 = new google.maps.places.Autocomplete(instance.$refs.autocomplete1, {
+    types: ['geocode']
+  });
+  instance.autocomplete2 = new google.maps.places.Autocomplete(instance.$refs.autocomplete2, {
+    types: ['geocode']
+  });
+  instance.autocomplete3 = new google.maps.places.Autocomplete(instance.$refs.autocomplete3, {
+    types: ['geocode']
+  });
+  addPlaceListener(instance, 'autocomplete0');
+  addPlaceListener(instance, 'autocomplete1');
+  addPlaceListener(instance, 'autocomplete2');
+  addPlaceListener(instance, 'autocomplete3');
+}
